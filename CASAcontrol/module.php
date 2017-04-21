e362b3daceb80ee26ac473ea0df4f7d6e9c25e13
JohBier <j.bier89@gmx.de>
2017-04-21 20:54:08 +0200

    hi

--- CASAcontrol/module.php
+++ CASAcontrol/module.php
@@ -28,7 +28,7 @@
 				IPS_SetName($VarID_Switch, "Switch");
 				IPS_SetParent($VarID_Switch, $this->InstanceID);
 			//}
-			$eid_on = @IPS_GetObjectIDByIdent("Switch ON", $this->InstanceID);
+			$eid_on = @IPS_GetObjectIDByIdent("SwitchON", $this->InstanceID);
 			if ($eid_on === false){ 
 				$eid_on = IPS_CreateEvent(0);
 				IPS_SetParent($eid_on, $this->InstanceID);
